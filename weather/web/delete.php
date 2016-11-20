6d08df4fb25f0f4790e6a4bd0f8e09e36bc2f30b
MikeVV <w.nanthakon@gmail.com>
2016-11-21 01:19:33 +0700

    weather

---
+++ weather/web/delete.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 21/11/2559
+ * Time: 0:57
+ */
---
+++ weather/web/load.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 20/11/2559
+ * Time: 23:48
+ */
